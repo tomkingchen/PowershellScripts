112e1dd813a14d4a133face41be4c2325fffda31
tomkingchen <30789036+tomkingchen@users.noreply.github.com>
2018-02-20 15:42:33 +1100

    Add files via upload

---
+++ Config-New-VM - public.ps1
@@ -0,0 +1,52 @@
+﻿<#
+.SYNOPSIS
+Carry out initial configuration based on new server build checklist tasks
+
+#>
+param( 
+	    [Parameter(Mandatory=$false)] 
+	    [String]$serverName, 
+	    [Parameter(Mandatory=$true)] 
+	    [String]$IPAddress,
+        [Parameter(Mandatory=$true)] 
+	    [String]$defaultGW,
+        [Parameter(Mandatory=$true)] 
+	    [String]$DNSServers
+	  ) 
+
+
+# Configure network interface
+$nic = Get-NetAdapter -Name "Ethernet"
+# Disable DHCP
+$nic |Set-NetIPInterface -Dhcp Disabled
+# Set static IP
+$nic |New-NetIPAddress -IPAddress $IPAddress -PrefixLength 24 -DefaultGateway $defaultGW
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $DNSServers
+
+# Disable Guest account
+Get-localuser Guest |Disable-LocalUser
+
+# Rename admin account
+Rename-LocalUser Administrator -NewName srv_admin
+
+# Change CD ROM Driver letter to Z:
+$drv = Get-WmiObject win32_volume -Filter 'DriveLetter ="D:"'
+$drv.DriveLetter = "Z:"
+$DRV.Put()|out-null
+
+# Set Security Eventlog retention to 60 days and size to 100MB
+Limit-EventLog -LogName Security -RetentionDays 60 -MaximumSize 100MB -OverflowAction OverwriteOlder
+
+# Rename computer name
+Try
+{
+    Rename-Computer -NewName $serverName 
+}
+Catch
+{
+    Echo "Server name will not be changed"
+}
+
+# Join to the domain
+$ADCred = Get-Credential
+Add-Computer -DomainName "Contoso.com" -OUPath "OU=Servers,DC=Contoso,DC=com" -Credential $ADCred
