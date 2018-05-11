bfc40d8b1288b4b51ba974d1bc67d96c9793e03d
tomkingchen <30789036+tomkingchen@users.noreply.github.com>
2018-05-11 14:48:42 +1000

    add new file

--- Add-EmailAlias.ps1
+++ Add-EmailAlias.ps1
@@ -1,11 +1,14 @@
-﻿# Script to find out recipients without HansenCX.com namespace
-# Update HSNTECH Parent Domain
+﻿# Use the script to add a new Email alias address to user mailboxes in Office 365
+# Find out recipients without @contosoNew.com namespace in the alias
+# Add the alias if found
+
+# Update Parent Domain
 foreach ($recipient in (Get-Recipient -resultsize unlimited -filter {EmailAddressPolicyEnabled -eq $false -and RecipientTypeDetails -eq "RemoteUserMailbox"})) {
-    if ($recipient.EmailAddresses | ? {$_ -like "*hansencx.com"}) {
+    if ($recipient.EmailAddresses | Where-Object {$_ -like "*contoso.com"}) {
         #do nothing
     } else {
         $EmailAddr = $recipient.primarysmtpaddress
-        $newEmailAddr = $EmailAddr -replace "@hsntech.com","@hansencx.com"
+        $newEmailAddr = $EmailAddr -replace "@contoso.com","@contosoNew.com"
         Try {
                 # Force the exception to stop the command and catch the error
                 $ErrorActionPreference = "Stop"
@@ -17,21 +20,20 @@ foreach ($recipient in (Get-Recipient -resultsize unlimited -filter {EmailAddres
                 Write-Host "Update $EmailAddr failed!" -ForegroundColor Red
                 Write-Host "Error Details: $ErrorMsg" -ForegroundColor Red
         }
-
     }
 }
 
 # Update US Child Domain
-foreach ($recipient in (Get-Recipient -resultsize unlimited -filter {EmailAddressPolicyEnabled -eq $false -and RecipientTypeDetails -eq "RemoteUserMailbox"} -DomainController hsnCBD-dc01.us.hsntech.int)) {
-    if ($recipient.EmailAddresses | ? {$_ -like "*hansencx.com"}) {
+foreach ($recipient in (Get-Recipient -resultsize unlimited -filter {EmailAddressPolicyEnabled -eq $false -and RecipientTypeDetails -eq "RemoteUserMailbox"} -DomainController USDC1.us.contoso.com)) {
+    if ($recipient.EmailAddresses | Where-Object {$_ -like "*contosoNew.com"}) {
         #do nothing
     } else {
         $EmailAddr = $recipient.primarysmtpaddress
-        $newEmailAddr = $EmailAddr -replace "@hsntech.com","@hansencx.com"
+        $newEmailAddr = $EmailAddr -replace "@contoso.com","@contosoNew.com"
         Try {
                 # Force the exception to stop the command and catch the error
                 $ErrorActionPreference = "Stop"
-                Set-RemoteMailbox -Identity $EmailAddr -EmailAddresses @{Add=$newEmailAddr} -DomainController hsncbd-dc01.us.hsntech.int
+                Set-RemoteMailbox -Identity $EmailAddr -EmailAddresses @{Add=$newEmailAddr} -DomainController USDC1.us.consoto.com
                 Write-Host "$EmailAddr is updated" -ForegroundColor Green
             }
         Catch{
@@ -39,50 +41,5 @@ foreach ($recipient in (Get-Recipient -resultsize unlimited -filter {EmailAddres
                 Write-Host "Update $EmailAddr failed!" -ForegroundColor Red
                 Write-Host "Error Details: $ErrorMsg" -ForegroundColor Red
         }
-
-    }
-}
-
-# Update NZ Child Domain
-foreach ($recipient in (Get-Recipient -resultsize unlimited -filter {EmailAddressPolicyEnabled -eq $false -and RecipientTypeDetails -eq "RemoteUserMailbox"} -DomainController hsnDON-NZdc03.NZ.hsntech.int)) {
-    if ($recipient.EmailAddresses | ? {$_ -like "*hansencx.com"}) {
-        #do nothing
-    } else {
-        $EmailAddr = $recipient.primarysmtpaddress
-        $newEmailAddr = $EmailAddr -replace "@hsntech.com","@hansencx.com"
-        Try {
-                # Force the exception to stop the command and catch the error
-                $ErrorActionPreference = "Stop"
-                Set-RemoteMailbox -Identity $EmailAddr -EmailAddresses @{Add=$newEmailAddr} -DomainController hsnDON-NZdc03.NZ.hsntech.int
-                Write-Host "$EmailAddr is updated" -ForegroundColor Green
-            }
-        Catch{
-                $ErrorMsg = $_.Exception.Message
-                Write-Host "Update $EmailAddr failed!" -ForegroundColor Red
-                Write-Host "Error Details: $ErrorMsg" -ForegroundColor Red
-        }
-
-    }
-}
-
-# Update CN Child Domain
-foreach ($recipient in (Get-Recipient -resultsize unlimited -filter {EmailAddressPolicyEnabled -eq $false -and RecipientTypeDetails -eq "RemoteUserMailbox"} -DomainController hsnsha-dc01.cn.hsntech.int)) {
-    if ($recipient.EmailAddresses | ? {$_ -like "*hansencx.com"}) {
-        #do nothing
-    } else {
-        $EmailAddr = $recipient.primarysmtpaddress
-        $newEmailAddr = $EmailAddr -replace "@hsntech.com","@hansencx.com"
-        Try {
-                # Force the exception to stop the command and catch the error
-                $ErrorActionPreference = "Stop"
-                Set-RemoteMailbox -Identity $EmailAddr -EmailAddresses @{Add=$newEmailAddr} -DomainController hsnsha-dc01.cn.hsntech.int
-                Write-Host "$EmailAddr is updated" -ForegroundColor Green
-            }
-        Catch{
-                $ErrorMsg = $_.Exception.Message
-                Write-Host "Update $EmailAddr failed!" -ForegroundColor Red
-                Write-Host "Error Details: $ErrorMsg" -ForegroundColor Red
-        }
-
     }
 }
